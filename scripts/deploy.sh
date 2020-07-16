33223c9c028b736f51c31e641632c6c0c1f72ae6
Hiren Joshi <hirenj@gmail.com>
2020-07-16 15:08:47 +0200

    Add script to generate resources conf

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,8 +16,9 @@ all_roles = {}
 
 parser = argparse.ArgumentParser(description='Deploy Lambdas to AWS')
 
-parser.add_argument('zipfile',metavar='zipfile', help='Zipfile for compiled lambda')
+parser.add_argument('zipfile',metavar='zipfile', nargs='?', help='Zipfile for compiled lambda')
 parser.add_argument('resources',metavar='resources', nargs='?', help='resources.conf.json file to deploy to')
+parser.add_argument('--print-resources', action='store_true', help='Print resources.conf.json')
 parser.add_argument('--force', action='store_true', help='Force deploy')
 
 args = parser.parse_args()
@@ -26,23 +27,19 @@ input_zip = args.zipfile
 resources_file = args.resources
 force = args.force
 
-if resources_file is None and 'STACK' not in os.environ and 'AWS_REGION' not in os.environ:
-  print("Missing resources file or environment variables STACK and AWS_REGION")
-  sys.exit(1)
-
 def remove_from_zip(zipfname, *filenames):
-    tempdir = tempfile.mkdtemp()
-    try:
-        tempname = os.path.join(tempdir, 'new.zip')
-        with zipfile.ZipFile(zipfname, 'r') as zipread:
-            with zipfile.ZipFile(tempname, 'w') as zipwrite:
-                for item in zipread.infolist():
-                    if item.filename not in filenames:
-                        data = zipread.read(item.filename)
-                        zipwrite.writestr(item, data)
-        shutil.move(tempname, zipfname)
-    finally:
-        shutil.rmtree(tempdir)
+  tempdir = tempfile.mkdtemp()
+  try:
+    tempname = os.path.join(tempdir, 'new.zip')
+    with zipfile.ZipFile(zipfname, 'r') as zipread:
+      with zipfile.ZipFile(tempname, 'w') as zipwrite:
+        for item in zipread.infolist():
+          if item.filename not in filenames:
+            data = zipread.read(item.filename)
+            zipwrite.writestr(item, data)
+    shutil.move(tempname, zipfname)
+  finally:
+    shutil.rmtree(tempdir)
 
 def combine_resources(resources):
 
@@ -78,6 +75,19 @@ def get_stack_resources(stack):
   return combine_resources(json.loads(json_text.decode('utf8'))['StackResourceSummaries'])
 
 def main():
+
+  if resources_file is None and 'STACK' not in os.environ and 'AWS_REGION' not in os.environ:
+    print("Missing resources file or environment variables STACK and AWS_REGION")
+    parser.print_help()
+    sys.exit(1)
+
+  if input_zip is None and args.print_resources:
+    print(json.dumps(get_stack_resources(os.environ['STACK'])))
+    sys.exit(0)
+
+  if input_zip is None:
+    parser.print_help()
+    sys.exit(1)
 
   temp_dir = tempfile.mkdtemp()
   target_zip = os.path.join(temp_dir, os.path.basename(input_zip))
