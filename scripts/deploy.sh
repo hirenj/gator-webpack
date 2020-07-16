5ca1d078593a696db9be1870fe26df75ef9e15d4
Hiren Joshi <hirenj@gmail.com>
2020-07-16 14:50:14 +0200

    Make deploy script not rely upon resources.conf.json

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,13 +10,14 @@ import subprocess
 import tempfile
 import shutil
 import zipfile
+import datetime
 
 all_roles = {}
 
 parser = argparse.ArgumentParser(description='Deploy Lambdas to AWS')
 
 parser.add_argument('zipfile',metavar='zipfile', help='Zipfile for compiled lambda')
-parser.add_argument('resources',metavar='resources', help='resources.conf.json file to deploy to')
+parser.add_argument('resources',metavar='resources', nargs='?', help='resources.conf.json file to deploy to')
 parser.add_argument('--force', action='store_true', help='Force deploy')
 
 args = parser.parse_args()
@@ -24,6 +25,10 @@ args = parser.parse_args()
 input_zip = args.zipfile
 resources_file = args.resources
 force = args.force
+
+if resources_file is None and 'STACK' not in os.environ and 'AWS_REGION' not in os.environ:
+  print("Missing resources file or environment variables STACK and AWS_REGION")
+  sys.exit(1)
 
 def remove_from_zip(zipfname, *filenames):
     tempdir = tempfile.mkdtemp()
@@ -39,6 +44,39 @@ def remove_from_zip(zipfname, *filenames):
     finally:
         shutil.rmtree(tempdir)
 
+def combine_resources(resources):
+
+  resource_keys = {
+    'AWS::Lambda::Function': 'functions',
+    'AWS::S3::Bucket' : 'buckets',
+    'AWS::DynamoDB::Table' : 'tables',
+    'AWS::StepFunctions::StateMachine' : 'stepfunctions',
+    'AWS::SQS::Queue' : 'queue',
+    'AWS::Events::Rule' : 'rule'
+  }
+  results = {
+    'functions' : {},
+    'buckets' : {},
+    'tables' : {},
+    'stepfunctions' : {},
+    'queue' : {},
+    'rule'  : {}
+  }
+
+  for resource in resources:
+    if resource['ResourceType'] in resource_keys:
+      wanted_key = resource_keys[ resource['ResourceType'] ]
+      results[wanted_key][ resource['LogicalResourceId'] ] = resource['PhysicalResourceId']
+
+  results['stack'] = os.environ['STACK']
+  results['timestamp'] = datetime.datetime.now().isoformat()
+
+  return results
+
+def get_stack_resources(stack):
+  json_text = subprocess.check_output(['aws','cloudformation','list-stack-resources','--region',os.environ['AWS_REGION'],'--stack-name', os.environ['STACK'] ])
+  return combine_resources(json.loads(json_text.decode('utf8'))['StackResourceSummaries'])
+
 def main():
 
   temp_dir = tempfile.mkdtemp()
@@ -46,14 +84,20 @@ def main():
   shutil.copy2(input_zip, target_zip)
 
   resource_data = {}
-  if os.path.isfile(resources_file):
-    with open(resources_file) as f:
+  local_resource_file = resources_file
+
+  if local_resource_file is not None and os.path.isfile(local_resource_file):
+    with open(local_resource_file) as f:
       all_resource_data = json.load(f)
       resource_data = all_resource_data['functions']
       region = all_resource_data['region']
   else:
-    print('Could not find resources file')
-    sys.exit(1)
+    all_resource_data = get_stack_resources(os.environ['STACK'])
+    region = os.environ['AWS_REGION']
+    resource_data = all_resource_data['functions']
+    local_resource_file = os.path.join(temp_dir, 'resources.conf.json')
+    with open(local_resource_file, 'w') as f:
+      json.dump(all_resource_data, f)
 
   function_name = os.path.basename(input_zip).split('-')[0]
   new_version = os.path.splitext('-'.join(os.path.basename(input_zip).split('-')[1:]))[0]
@@ -65,7 +109,7 @@ def main():
   remove_from_zip(target_zip,'resources.conf.json')
 
   zip = zipfile.ZipFile(target_zip,'a')
-  zip.write(resources_file,'resources.conf.json')
+  zip.write(local_resource_file,'resources.conf.json')
   zip.close()
 
 
